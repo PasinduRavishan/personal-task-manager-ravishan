f651d1b8f5719dc92569a33c42342029cb06c448
Pasindu Ravishan <pasinduravishan88@gmail.com>
2025-08-28 15:03:00 +0530

    chore: remove .next build folder from repo

--- .next/static/chunks/Desktop_Scheduly_scheduly_src_app_favicon_ico_mjs_b0c6ab55._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/09c65_next_dist_cf4621ca._.js"
-],
-    source: "dynamic"
-});
