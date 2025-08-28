fc1f4a81f5614aeb4b980d4143481c3c1f2317ed
Pasindu Ravishan <pasinduravishan88@gmail.com>
2025-08-28 15:13:17 +0530

    chore: remove .next folder from repo

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HFF4YnrzReB2OCdvUvFAAUdotdYB8pNB7h3nT1ulvac=\"\n}"
--- .next/static/chunks/Desktop_Scheduly_scheduly_src_app_page_tsx_35387385._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/09c65_next_dist_5eb0ec9d._.js"
-],
-    source: "dynamic"
-});
